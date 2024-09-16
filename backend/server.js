3d567b1de34fff31d35ac448e2d9b58b09fe1d94
Prajyot Shinde <prajyotshinde60@gmail.com>
2024-09-16 10:22:07 +0530

    Add new dependencies and configuration files

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+// const express = require('express'); 
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+import authRoutes from './routes/auth.route.js';
+import { connectDB } from './lib/db.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // to accept json data from client
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
